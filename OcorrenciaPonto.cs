d3e61a28fc045180f941a947c437c8f120deb29d
Murilo Duarte Henriques <103260127+mdhenriques@users.noreply.github.com>
2023-10-26 20:47:17 -0300

    Add files via upload

---
+++ OcorrenciaPonto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class OcorrenciaPonto
+    {
+        public String matricula;
+        public String data;
+        public String hora;
+        public String filler;
+
+        public void setMatricula(String _matricula) { matricula = _matricula; }
+        public void setData(String _data) { data = _data; }
+        public void setHora(String _hora) { hora = _hora; }
+        public void setFiller(String _filler) { filler = _filler; }
+        
+
+        public String getMatricula() { return matricula; }
+        public String getData() { return data; }
+        public String getHora() { return hora; }
+        public String getFiller() { return filler; }
+        
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int index = 0;
+            string linha;
+
+            StreamReader arquivoDePonto = new StreamReader("abril2017.txt");
+
+            
+            OcorrenciaPonto registroPonto = new OcorrenciaPonto();
+            List<OcorrenciaPonto> listaDePontos = new List<OcorrenciaPonto>();
+
+
+            //METODO UTILIZANDO SUBSTRING
+            //while ((linha = arquivoDePonto.ReadLine()) != null)
+            //{
+            //    listaDePontos.Add(new OcorrenciaPonto());
+
+            //    listaDePontos[index].matricula = linha.Substring(0, 15);
+            //    listaDePontos[index].data = linha.Substring(15, 6);
+            //    listaDePontos[index].hora = linha.Substring(21, 4);
+            //    listaDePontos[index].filler = linha.Substring(25);
+
+            //    index++;
+            //}
+            //arquivoDePonto.Close();
+
+            
+
+            //METODO UTILIZANDO REGEX
+            while ((linha = arquivoDePonto.ReadLine()) != null)
+            {
+                Regex expressao = new Regex(@"(?<matricula>.{15})(?<data>\d{6})(?<hora>\d{4})(?<filler>\d{8})");
+                Match dados = expressao.Match(linha);
+                listaDePontos.Add(new OcorrenciaPonto());
+
+                listaDePontos[index].setMatricula(dados.Groups["matricula"].Value);
+                listaDePontos[index].setData(dados.Groups["data"].Value);
+                listaDePontos[index].setHora(dados.Groups["hora"].Value);
+                listaDePontos[index].setFiller(dados.Groups["filler"].Value);
+
+                index++;
+            }
+            arquivoDePonto.Close();
+
+            
+            
+
+            TextWriter arquivoXML = new StreamWriter("PontoAbril2017regex.xml");
+            XmlSerializer objetoNaoSerializado = new XmlSerializer(listaDePontos.GetType());
+
+            objetoNaoSerializado.Serialize(arquivoXML, listaDePontos);
+            arquivoXML.Close();
+
+        }
+    }
+}
